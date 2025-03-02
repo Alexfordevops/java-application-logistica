3e10fe97c83b5978fbc2505e0ae27c656dcdcb24
Matheus Alexandre <matheusalexandre.in@gmail.com>
2025-03-02 09:24:47 -0300

    criacao do projeto base angular

---
+++ backend/app/src/main/java/application/core/app/models/Product.java
@@ -0,0 +1,24 @@
+package application.core.app.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private Integer quantity;
+    private double price;
+    private LocalDateTime CreationDate = LocalDateTime.now();
+}
